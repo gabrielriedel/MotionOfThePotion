e4dafea3e04762546ce870a3f9e1f5c14f004f15
gabe riedel <gaberiedel@gabes-MacBook-Pro.local>
2024-04-10 13:41:21 -0700

    bug fixes

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -23,7 +23,7 @@ def get_catalog():
                     "sku": "GREEN_POTION",
                     "name": "green potion",
                     "quantity": num_green_potions,
-                    "price": 60,
+                    "price": 30,
                     "potion_type": [0, 100, 0, 0],
                 }
             ]
